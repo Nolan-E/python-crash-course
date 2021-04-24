12d043ca79781bb1cc45ae477e5f011c79d75d30
Nolan Eckert <nolan.eckert@gmail.com>
2021-04-24 14:40:01 -0600

    Single, double and triple quotes. String literals.

---
+++ multi-line-strings.py
@@ -0,0 +1,11 @@
+singleQuote = 'Hello world'
+doubleQuote = "Hello world"
+tripleQuote = """start
+Hello world
+Hello world
+Hello world
+end"""
+
+print(singleQuote)
+print(doubleQuote)
+print(tripleQuote)
