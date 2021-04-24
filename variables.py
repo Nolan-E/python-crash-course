c31e682b0d41fd9e1276c9ac33eefb3f0be297c8
Nolan Eckert <nolan.eckert@gmail.com>
2021-04-24 14:15:33 -0600

    Add variables.py

---
+++ variables.py
@@ -0,0 +1,13 @@
+name = "Thomas"
+print("Hello, " + name + ", it's very nice to meet you.")
+
+taxRate = 0.14
+subtotal = 20
+tax = subtotal * taxRate
+total = subtotal + tax
+print("Your total bill is:")
+print(total)
+
+travelDestinations = ["Bogota", "Buenos Aires", "Munich", "Glasgow", "Reykjavik"]
+for city in travelDestinations:
+    print(city)
