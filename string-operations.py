5cec196e7590b37a3563a096e74e5230e2ea2650
Nolan Eckert <nolan.eckert@gmail.com>
2021-04-26 23:10:31 -0600

    Add string operations

---
+++ string-operations.py
@@ -0,0 +1,15 @@
+first = "Brittany"
+last = "Eckert"
+full = first + ' ' + last
+
+print(2+2)
+print('2' + '2')
+
+print(full)
+print(full[2])
+print(full[-2])
+print(full[0:4])
+
+different = full.replace('tany', '')
+
+print(different)
